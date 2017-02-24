1f95c13352aae0d637242e1d211398126e94d6a8
jnishida <nishida@allora-inc.jp>
2017-02-25 07:25:03 +0900

    read config

---
+++ src/main/java/jp/allora_inc/microsoft/graph/App2Config.java
@@ -0,0 +1,23 @@
+package jp.allora_inc.microsoft.graph;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+import lombok.Value;
+
+@Value
+public class App2Config {
+	private String redirectUri;
+	private String clientId;
+
+	public App2Config() throws IOException {
+		Resource resource = new ClassPathResource("/application.properties");
+		Properties props = PropertiesLoaderUtils.loadProperties(resource);
+		redirectUri = props.getProperty("app2.redirect.uri");
+		clientId = props.getProperty("app2.client.id");
+	}
+}
--- src/main/java/jp/allora_inc/microsoft/graph/App2OAuthParamBuilder.java
+++ src/main/java/jp/allora_inc/microsoft/graph/App2OAuthParamBuilder.java
@@ -1,27 +1,24 @@
 package jp.allora_inc.microsoft.graph;
 
-import java.io.UnsupportedEncodingException;
+import java.io.IOException;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class App2OAuthParamBuilder extends HttpParamBuilder {
-	@Value("${app2.redirect.uri}")
-	private String redirectUri;
-
-	@Value("${qpp2.client.id}")
-	private String clientId;
-
-	private App2OAuthParamBuilder() throws UnsupportedEncodingException {
+	private App2OAuthParamBuilder() throws IOException {
+		App2Config config = new App2Config();
 		append("grant_type", "authorization_code");
-		append("redirect_uri", redirectUri);
-		append("client_id", clientId);
+		append("redirect_uri", config.getRedirectUri(), "UTF-8");
+		append("client_id", config.getClientId(), "UTF-8");
 	}
 
-	public static App2OAuthParamBuilder withCode(String code) throws UnsupportedEncodingException {
-		return new App2OAuthParamBuilder().code(code);
+	public static App2OAuthParamBuilder withCode(String code) throws IOException {
+		App2OAuthParamBuilder me = new App2OAuthParamBuilder();
+		return me.code(code);
 	}
 
-	private App2OAuthParamBuilder code(String value) {
+	private App2OAuthParamBuilder code(String value) throws IOException {
 		append("code", value);
 		return this;
 	}
